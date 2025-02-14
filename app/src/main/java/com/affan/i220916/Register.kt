62caa2e584a56a3681934afeac6c45e8a73cd2f7
Affan-Swati <i220916@nu.edu.pk>
2025-02-14 16:48:30 +0500

    feat: designed sign-up page

--- app/src/main/java/com/affan/i220916/Register.kt
+++ app/src/main/java/com/affan/i220916/Register.kt
@@ -2,28 +2,26 @@ package com.affan.i220916
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Add a delay before starting the login activity
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
-        }, 2000) // 2000 milliseconds = 2 seconds
+        val loginButton = findViewById<TextView>(R.id.login_button)
+        loginButton.setOnClickListener {
+            finish()
+        }
     }
 }
